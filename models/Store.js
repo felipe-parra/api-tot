5dd93f129e10378cc3c6ec8c33684b844e61f583
Felipe <fpr29.10@gmail.com>
2019-09-17 00:52:54 -0500

    adding model field

--- models/Store.js
+++ models/Store.js
@@ -1,27 +1,35 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema } = require('mongoose');
 
-const storeSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  location: {
-    type: {
-      type: String,
-      enum: ["Point"]
-      // required: true
-    },
-    coordinates: {
-      type: [Number]
-      // required: true
-    }
-  },
-  address: {
-    type: String
-    // required: true,
-  }
-});
+const storeSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true
+		},
+		location: {
+			type: {
+				type: String,
+				enum: [ 'Point' ]
+				// required: true
+			},
+			coordinates: {
+				type: [ Number ]
+				// required: true
+			}
+		},
+		address: {
+			type: String
+			// required: true,
+		},
+		model: {
+			type: String
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
-module.exports = model("Store", storeSchema);
+module.exports = model('Store', storeSchema);
