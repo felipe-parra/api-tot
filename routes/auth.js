d7f8335160c8aba715d6e1520b06d3d798cabdfb
Felipe <fpr29.10@gmail.com>
2019-09-17 00:54:28 -0500

    rename endpoints name, dismiss edit, delete, create

--- routes/auth.js
+++ routes/auth.js
@@ -1,50 +1,50 @@
-const router = require("express").Router();
-const ensureLogin = require("connect-ensure-login");
-const passport = require("../config/passport");
-const User = require("../models/User");
-const Store = require("../models/Store");
+const router = require('express').Router();
+const ensureLogin = require('connect-ensure-login');
+const passport = require('../config/passport');
+const User = require('../models/User');
+const Store = require('../models/Store');
 
-router.get("/", (req, res) => {
-  User.find()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+router.get('/', (req, res) => {
+	User.find()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 
-router.get("/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+router.get('/:id', (req, res) => {
+	User.findById(req.params.id)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 
-router.post("/register", (req, res) => {
-  User.register({ ...req.body }, req.body.password)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(error => {
-      res.status(400).json("Error: " + error);
-    });
+router.post('/', (req, res) => {
+	User.register({ ...req.body }, req.body.password)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((error) => {
+			res.status(400).json('Error: ' + error);
+		});
 });
 
-router.get("/register", (req, res) => {
-  const stores = Store.find();
-  res.render("register", { stores });
-});
+// router.get("/register", (req, res) => {
+//   const stores = Store.find();
+//   res.render("register", { stores });
+// });
 
-router.get("/login", (req, res) => {
-  res.render("form");
-});
+// router.get("/login", (req, res) => {
+//   res.render("form");
+// });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  res.json({ loggedUser: req.user });
+router.post('/login', passport.authenticate('local'), (req, res) => {
+	res.json({ loggedUser: req.user });
 });
 // router.post(
 //   "/login",
@@ -54,28 +54,36 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
 //   })
 // );
 
-router.get(
-  "/profile",
-  ensureLogin.ensureLoggedIn({ failureRedirect: "/api/user/login" }),
-  (req, res) => {
-    res.render("profile", { loggedUser: req.user });
-  }
-);
-
-router.get("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/api/user/login");
+router.get('/profile', ensureLogin.ensureLoggedIn({ failureRedirect: '/api/user/login' }), (req, res) => {
+	res.render('profile', { loggedUser: req.user });
 });
 
-router.patch("/edit/:id", (req, res) => {
-  const { id } = req.params;
+router.get('/logout', (req, res) => {
+	req.logOut();
+	res.redirect('/api/user/login');
+});
 
-  User.findByIdAndUpdate(id, req.body, { new: true })
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+router.patch('/:id', (req, res) => {
+	const { id } = req.params;
+
+	User.findByIdAndUpdate(id, req.body, { new: true })
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
+});
+
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+
+	User.findByIdAndDelete(id)
+		.then((user) => {
+			res.status(200).json('User deleted successfully');
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 module.exports = router;
--- routes/order.js
+++ routes/order.js
@@ -1,19 +1,19 @@
-const router = require("express").Router();
-const Order = require("../models/Order");
-const User = require("../models/User");
-const Store = require("../models/Store");
+const router = require('express').Router();
+const Order = require('../models/Order');
+const User = require('../models/User');
+const Store = require('../models/Store');
 
-router.get("/", (req, res) => {
-  Order.find()
-    .populate("senderStore", "name")
-    .populate("storeOrder", "name")
-    .populate("userOrdered", "username")
-    .then(orders => {
-      res.status(200).json(orders);
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+router.get('/', (req, res) => {
+	Order.find()
+		.populate('senderStore', 'name')
+		.populate('storeOrder', 'name')
+		.populate('userOrdered', 'username')
+		.then((orders) => {
+			res.status(200).json(orders);
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 // router.get('/:storeId', (req, res) => {
 //   const {storeId} = req.params
@@ -26,62 +26,62 @@ router.get("/", (req, res) => {
 //   })
 // })
 
-router.get("/:id", (req, res) => {
-  Order.findById(req.params.id)
-    .populate("senderStore", "name")
-    .populate("storeOrder", "name")
-    .populate("userOrdered", "username")
-    .then(order => {
-      res.status(200).json(order);
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+router.get('/:id', (req, res) => {
+	Order.findById(req.params.id)
+		.populate('senderStore', 'name')
+		.populate('storeOrder', 'name')
+		.populate('userOrdered', 'username')
+		.then((order) => {
+			res.status(200).json(order);
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 
-router.post("/create/:id", async (req, res) => {
-  const { id } = req.params;
+router.post('/:userId', async (req, res) => {
+	const { userId } = req.params;
 
-  try {
-    const user = await User.findById(id);
-    Order.create({
-      ...req.body,
-      storeOrder: user.store,
-      userOrdered: user._id
-    })
-      .then(order => {
-        res.status(200).json("Created successfully");
-      })
-      .catch(err => {
-        res.status(400).json("Error: " + err);
-      });
-  } catch (error) {
-    res.status(400).json("Error: " + error);
-  }
+	try {
+		const user = await User.findById(userId);
+		Order.create({
+			...req.body,
+			storeOrder: user.store,
+			userOrdered: user._id
+		})
+			.then((order) => {
+				res.status(200).json('Created successfully');
+			})
+			.catch((err) => {
+				res.status(400).json('Error: ' + err);
+			});
+	} catch (error) {
+		res.status(400).json('Error: ' + error);
+	}
 });
 
-router.patch("/edit/:id", (req, res) => {
-  const { id } = req.params;
+router.patch('/:id', (req, res) => {
+	const { id } = req.params;
 
-  Order.findByIdAndUpdate(id, req.body, { new: true })
-    .then(order => {
-      res.status(200).json("Updated successfully");
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+	Order.findByIdAndUpdate(id, req.body, { new: true })
+		.then((order) => {
+			res.status(200).json('Updated successfully');
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 
-router.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
 
-  Order.findOneAndDelete(id)
-    .then(() => {
-      res.status(200).json("Deleted successfully");
-    })
-    .catch(err => {
-      res.status(400).json("Error: " + err);
-    });
+	Order.findByIdAndDelete(id)
+		.then(() => {
+			res.status(200).json('Deleted successfully');
+		})
+		.catch((err) => {
+			res.status(400).json('Error: ' + err);
+		});
 });
 
 module.exports = router;
